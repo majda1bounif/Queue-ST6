138a424d32007b68316dba233e2a3eddaf7abe62
Majda Bounif <majda1bounif@gmail.com>
2020-05-16 14:50:28 +0300

    testing software 6

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,142 @@
+﻿using NUnit.Framework;
+using ConsoleApp1;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test()]
+        public void EnqueueTest()
+        {
+            QueueL<int> q = new QueueL<int>();
+            q.Enqueue(1);
+            q.Enqueue(2);
+            q.Enqueue(3);
+            string s = "";
+            foreach (var i in q)
+                s += i.ToString();
+            string expected = "123";
+            Assert.AreEqual(s, expected);
+        }
+        [Test()]
+        public void DequeueTest()
+        {
+            QueueL<int> q = new QueueL<int>();
+            q.Enqueue(1);
+            q.Enqueue(2);
+            q.Enqueue(3);
+            q.Dequeue();
+            string s = "";
+            foreach (var i in q)
+                s += i.ToString();
+            string expected = "23";
+            Assert.AreEqual(s, expected);
+        }
+        [Test()]
+        public void FirstPassTest()
+        {
+            QueueL<int> q = new QueueL<int>();
+            q.Enqueue(1);
+            q.Enqueue(2);
+            q.Enqueue(3);
+            int actual = q.First;
+            int expected = 1;
+            Assert.AreEqual(actual, expected);
+        }
+        [Test()]
+        //[ExpectedException(typeof(InvalidOperationException))]
+        public void FirstExcTest()
+        {
+            QueueL<int> q = new QueueL<int>();
+            int actual;
+            Assert.Throws<InvalidOperationException>(
+                () => { actual = q.First(); });
+
+        }
+        [Test()]
+        public void LastPassTest()
+        {
+            QueueL<int> q = new QueueL<int>();
+            q.Enqueue(1);
+            q.Enqueue(2);
+            q.Enqueue(3);
+            int actual = q.Last;
+            int expected = 3;
+            Assert.AreEqual(actual, expected);
+        }
+        [Test()]
+        //[ExpectedException(typeof(InvalidOperationException))]
+        public void LastExcTest()
+        {
+            QueueL<int> q = new QueueL<int>();
+            int actual;
+            //string actual;
+            Assert.Throws<InvalidOperationException>(
+                () => { actual = q.Last(); });
+
+        }
+        [Test()]
+        public void CountTest()
+        {
+            QueueL<int> q = new QueueL<int>();
+            q.Enqueue(1);
+            q.Enqueue(2);
+            q.Enqueue(3);
+            int actual = q.Count;
+            int expected = 3;
+            Assert.AreEqual(actual, expected);
+        }
+        [Test()]
+        public void IsEmptyTest()
+        {
+            QueueL<int> q = new QueueL<int>();
+            q.Enqueue(1);
+            q.Enqueue(2);
+            q.Enqueue(3);
+            bool actual = q.IsEmpty;
+            bool expected = false;
+            Assert.AreEqual(actual, expected);
+        }
+        public void ClearTest()
+        {
+            QueueL<int> q = new QueueL<int>();
+            q.Enqueue(1);
+            q.Enqueue(2);
+            q.Enqueue(3);
+            q.Clear();
+            int actual = q.Count;
+            int expected = 0;
+            Assert.AreEqual(actual, expected);
+        }
+        [Test()]
+        public void ContainsTest()
+        {
+            QueueL<int> q = new QueueL<int>();
+            q.Enqueue(1);
+            q.Enqueue(2);
+            q.Enqueue(3);
+            bool actual = q.Contains(5);
+            bool expected = false;
+            Assert.AreEqual(actual, expected);
+        }
+        [Test()]
+        //[ExpectedException(typeof(InvalidOperationException))]
+        public void DequeueExcTest()
+        {
+            QueueL<int> q = new QueueL<int>();
+            int actual;
+            //string actual;
+            Assert.Throws<InvalidOperationException>(
+                () => { actual = q.Dequeue(); });
+            //QueueL<int> q = new QueueL<int>();
+            //q.Dequeue();
+        }
+    }
+}
---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Node<T>
+    {
+        public Node(T data)
+        {
+            Data = data;
+        }
+        public T Data { get; set; }
+        public Node<T> Next { get; set; }
+    }
+
+    public class QueueL<T> : IEnumerable<T>
+    {
+        Node<T> head; // the head of the 1st element
+        Node<T> tail; // the tail 
+        int count;
+        // adding in the queue
+        public void Enqueue(T data)
+        {
+            Node<T> node = new Node<T>(data);
+            Node<T> tempNode = tail;
+            tail = node;
+            if (count == 0)
+                head = tail;
+            else
+                tempNode.Next = tail;
+            count++;
+        }
+        // deleting from the queue
+        public T Dequeue()
+        {
+            if (count == 0)
+                throw new InvalidOperationException();
+            T output = head.Data;
+            head = head.Next;
+            count--;
+            return output;
+        }
+        // get the first element
+        public T First
+        {
+            get
+            {
+                if (IsEmpty)
+                    throw new InvalidOperationException();
+                return head.Data;
+            }
+        }
+        // get the last element
+        public T Last
+        {
+            get
+            {
+                if (IsEmpty)
+                    throw new InvalidOperationException();
+                return tail.Data;
+            }
+        }
+        public int Count { get { return count; } }
+        public bool IsEmpty { get { return count == 0; } }
+
+        public void Clear()
+        {
+            head = null;
+            tail = null;
+            count = 0;
+        }
+
+        public bool Contains(T data)
+        {
+            Node<T> current = head;
+            while (current != null)
+            {
+                if (current.Data.Equals(data))
+                    return true;
+                current = current.Next;
+            }
+            return false;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)this).GetEnumerator();
+        }
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            Node<T> current = head;
+            while (current != null)
+            {
+                yield return current.Data;
+                current = current.Next;
+            }
+        }
+    }
+}
